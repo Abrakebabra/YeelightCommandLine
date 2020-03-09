668c4f53104889cb247504dcdc10c082fe6ab1d4
Keith Lee <lee.mingkeith@gmail.com>
2020-03-09 18:24:55 +0900

    Initial Commit

---
+++ YeelightCommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YeelightCommandLine
+//
+//  Created by Keith Lee on 2020/03/09.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
